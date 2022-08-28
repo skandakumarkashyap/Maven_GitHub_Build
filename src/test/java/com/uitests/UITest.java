0e7e610380b2465234370049cd8c0a171bf75031
skanda kumar <skandakumarkashyap@gmail.com>
2022-08-28 12:44:20 +0530

    Code changes for parameters

--- src/test/java/com/uitests/UITest.java
+++ src/test/java/com/uitests/UITest.java
@@ -3,17 +3,30 @@ package com.uitests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.util.logging.Logger;
 
 public class UITest {
 
+    @Parameters("Browser")
     @Test
-    public void startBrowser() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+    public void startBrowser(String browserName) throws InterruptedException {
+
+        WebDriver driver = null;
+        if(browserName.contains("Chrome"))
+        {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        else if(browserName.contains("Edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+
         driver.manage().window().maximize();
         Thread.sleep(8000);
         driver.get("https://opensource-demo.orangehrmlive.com/");
